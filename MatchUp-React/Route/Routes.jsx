45bc900109d4e94d2f300c926cdd716ce614f00f
SadSloth29 <sadmansadik94@gmail.com>
2025-03-15 03:28:21 +0600

    Add route to user feed

--- MatchUp-React/Route/Routes.jsx
+++ MatchUp-React/Route/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
 import React from 'react'
+import UserFeed from '../Pages/UserFeed'
 
 
     const router=createBrowserRouter([
@@ -12,6 +13,10 @@ import React from 'react'
         {
             path: "/:type",
             element: <Login/>
+        },
+        {
+            path: "/users/:username",
+            element: <UserFeed/>
         }
     ]);
 
